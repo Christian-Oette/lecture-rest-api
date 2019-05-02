397b9dd14eef8aef6581634b2f342152cbc8d81d
Christian Oette <christian.oette@magicline.de>
2019-05-02 21:10:57 +0200

    Add WebSecurity Adapter

---
+++ src/main/java/de/oette/lecture/A06/controller/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,36 @@
+package de.oette.lecture.A06.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * pass in base64 -> dXNlcjpwYXNz
+ * curl -XGET localhost:8090/secure-demo/private -H "Authorization: Basic dXNlcjpwYXNz
+ */
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
