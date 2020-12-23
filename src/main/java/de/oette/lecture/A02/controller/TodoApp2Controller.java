a9b0a9f06a9f102742c30f33b1f0925c4680acc3
Christian Oette <christian.oette@web.de>
2020-12-23 13:30:22 +0100

    Update TodoApp2Controller.java

--- src/main/java/de/oette/lecture/A02/controller/TodoApp2Controller.java
+++ src/main/java/de/oette/lecture/A02/controller/TodoApp2Controller.java
@@ -37,8 +37,6 @@ public class TodoApp2Controller {
         currentTask.description = todo.description;
         currentTask.dueAt = todo.dueAt;
         currentTask.version++;
-
-        todos.put(position, todo);
     }
 
     @DeleteMapping
